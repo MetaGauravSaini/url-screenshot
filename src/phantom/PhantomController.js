14fa720534af91e57dbd9f1cdcd00782eb5dbdd9
gaurav-saini <gaurav.saini@metacube.com>
2019-02-25 17:32:00 +0530

    updated api - bulk thumbnail generation

--- src/phantom/PhantomController.js
+++ src/phantom/PhantomController.js
@@ -4,18 +4,14 @@ const bodyParser = require('body-parser');
 const PhantomService = require('./PhantomService');
 
 let router = express.Router();
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));  // for url encoded bodies
+router.use(bodyParser.json());    // for json encoded bodies
 
 router.get('/', (req, res) => {
     res.status(200).send('inside get route.');
     console.log('inside get route.');
 });
 
-router.post('/capture-screen', async (req, res) => {
-    let base64Data = await PhantomService.createScreenshotFromUrl();
-    res.status(200).send(base64Data);
-    console.log('inside post route.');
-});
+
 
 module.exports = router;
